da4a90cb714f88590678cbf6b26317b2f87dc6fc
rakesh468 <rakeshkumarg2000@gmail.com>
2022-02-18 05:12:08 -0800

    firrst commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Home.css";
 
 export function Home() {
   return (
     <div className="home">
-      <h1>Welcome to the world of Television Network</h1>
+     <img  className="home-image" src="https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmV0ZmxpeHxlbnwwfHwwfHw%3D&w=1000&q=80" alt="poster"/>
     </div>
   );
 }
