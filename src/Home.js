a220efb6613759042dbd4174f3c67b9ed3efae72
rakesh468 <rakeshkumarg2000@gmail.com>
2022-02-25 00:27:21 -0800

    first commit

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,6 @@ import "./Home.css";
 export function Home() {
   return (
     <div className="home">
-    {/* <h1> <span style={{color:"red"}}>Welcome to Netflix </span></h1> */}
     <img src="https://www.allconnect.com/wp-content/uploads/2020/02/netflix-review-hero-image-0224.png" alt="poster"className="poster-image"/>
     </div>
   );
